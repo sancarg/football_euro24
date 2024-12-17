c6d71291b9f3be30eb65e715df7285cc40bf4f69
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-17 12:46:45 +0300

    Update euro24_shot.py

--- euro24_shot.py
+++ euro24_shot.py
@@ -8,5 +8,5 @@ st.subheader("Select any team/player to see all their shots on the pitch!")
 
 # Loading the dataset
 df = pd.read_csv('euro24_shot_map.csv')
-df = df[df['type']].reset_index(drop=True)
+df = df[df['type']]
 df['location'] = df['location'].apply(json.loads)
