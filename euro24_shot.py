f05bd7aac5c6f2ff95c7997ba91d0ac64f0de015
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-17 12:31:36 +0300

    Update euro24_shot.py

--- euro24_shot.py
+++ euro24_shot.py
@@ -5,3 +5,8 @@ from mplsoccer import VerticalPitch
 
 st.title("⚽Euro 2024 Shot Map")
 st.subheader("Select any team/player to see all their shots on the pitch!")
+
+# Loading the dataset
+df = pd.read_csv('euro24_shot_map.csv')
+df = df[df['type']].reset_index(drop=True)
+df['location'] = df['location'].apply(json.loads)
