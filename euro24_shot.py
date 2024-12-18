1b8fba4ee65373dc91b2e710cfa9bbc980f6d2e8
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-18 15:44:49 +0300

    Update euro24_shot.py

--- euro24_shot.py
+++ euro24_shot.py
@@ -7,7 +7,7 @@ st.title("⚽Euro 2024 Shot Map")
 st.subheader("Select any team/player to see all their shots on the pitch!")
 
 # Loading the dataset
-df = pd.read_csv('euro24_shot_map.csv')
+df = pd.read_csv('euros_2024_shot_map.csv')
 df = df[df['type'] == 'Shot'].reset_index(drop=True)
 df['location'] = df['location'].apply(json.loads)
 team = st.selectbox('Select a team', df['team'].sort_values().unique(), index=None)
