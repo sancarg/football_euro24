3f7866bd8feb93baf885af3e37084443b447a510
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-18 16:16:15 +0300

    Update euro24_shot.py

--- euro24_shot.py
+++ euro24_shot.py
@@ -7,5 +7,5 @@ st.title("⚽Euro 2024 Shot Map")
 st.subheader("Select any team/player to see all their shots on the pitch!")
 
 # Loading the dataset
-df = pd.read_csv('euros_2024_shot_map.csv')
-st.table(df)
+df = pd.read_csv('euro24_shot_map.csv')
+
