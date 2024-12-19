185cf884c87ad79cd0a46ae1ae32c72dedd2fe1f
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-19 12:11:14 +0300

    Update deneme.py

--- deneme.py
+++ deneme.py
@@ -26,7 +26,7 @@ def update_player():
 
 # Select team 
 if st.session_state.team is not None:
-  selected_player = st.selectbox('Select a player', st.session_state.team['player'].sort_values().unique(), on_change=update_player, key='player')
+  selected_player = st.selectbox('Select a player', st.session_state.team.sort_values().unique(), on_change=update_player, key='player')
 else:
   selected_player = st.selectbox('Select a player', ['Please select a team first'], key='player')
 
