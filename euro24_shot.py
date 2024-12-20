39155eae8b2273fe7f188117d8688fd43a11475f
sancarg <94074200+sancarg@users.noreply.github.com>
2024-12-20 11:20:52 +0300

    Update euro24_shot.py

--- euro24_shot.py
+++ euro24_shot.py
@@ -3,29 +3,63 @@ import pandas as pd
 import json
 from mplsoccer import VerticalPitch
 
-st.title("⚽Euro 2024 Shot Map")
-st.subheader("Select any team/player to see all their shots on the pitch!")
+st.title("⚽ Euro 2024 Shot Map")
+st.subheader("Filter to any team then player to see all of their shots and goal-scoring shots!")
 
-# Loading the dataset
-df = pd.read_csv('euro24_shot_map.csv')
+# Read and prepare the data
+df = pd.read_csv('euros_2024_shot_map.csv')
+df = df[df['type'] == 'Shot'].reset_index(drop=True)
 df['location'] = df['location'].apply(json.loads)
 
-team = st.selectbox('Select a team', df['team'].sort_values().unique(), index=None)
-player = st.selectbox('Select a player', df[df['team'] == team]['player'].sort_values().unique(), index=None)
+# Print column names for debugging
+st.write("Columns in the DataFrame:", df.columns.tolist())
 
+# Initialize session state
+if 'team' not in st.session_state:
+    st.session_state.team = None
+if 'player' not in st.session_state:
+    st.session_state.player = None
+
+# Define callback functions
+def update_team():
+    st.session_state.team
+
+def update_player():
+    st.session_state.player
+
+# Select team
+selected_team = st.selectbox(
+    'Select a team',
+    df['team'].sort_values().unique(),
+    on_change=update_team,
+    key='team'
+)
+
+# Select player
+if st.session_state.team:
+    selected_player = st.selectbox(
+        'Select a player',
+        df[df['team'] == st.session_state.team]['player'].sort_values().unique(),
+        on_change=update_player,
+        key='player'
+    )
+
+# Function to filter data
 def filter_data(df, team, player):
     if team:
         df = df[df['team'] == team]
     if player:
         df = df[df['player'] == player]
-    
     return df
 
-filtered_df = filter_data(df, team, player)
+# Filter data for the selected player
+filtered_df = filter_data(df, st.session_state.team, st.session_state.player)
 
+# Create the pitch and plot shots
 pitch = VerticalPitch(pitch_type='statsbomb', half=True)
 fig, ax = pitch.draw(figsize=(10, 10))
 
+# Function to plot shots
 def plot_shots(df, ax, pitch):
     for x in df.to_dict(orient='records'):
         pitch.scatter(
@@ -35,10 +69,13 @@ def plot_shots(df, ax, pitch):
             s=1000 * x['shot_statsbomb_xg'],
             color='green' if x['shot_outcome'] == 'Goal' else 'white',
             edgecolors='black',
-            alpha=1 if x['type'] == 'goal' else .5,
-            zorder=2 if x['type'] == 'goal' else 1
+            alpha=1 if x['shot_outcome'] == 'Goal' else 0.5,
+            zorder=2 if x['shot_outcome'] == 'Goal' else 1
         )
 
+# Plot the filtered shots
 plot_shots(filtered_df, ax, pitch)
 
+# Display the pitch with shots
 st.pyplot(fig)
+
